1fc941b351ef2b20bad1d3a980a8bf639fcb12a2
Zulfiqor <siapaa249@gmail.com>
2025-03-10 15:15:39 +0700

    ✨

--- lib/core/network/api_clients.dart
+++ lib/core/network/api_clients.dart
@@ -2,6 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiClients {
   static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
-  static String get users => '$baseUrl/user';
+  static String get users => '$baseUrl/users';
   static String get login => '$baseUrl/login';
 }
